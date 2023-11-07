75ab8366c9b4ee8a6d11609017db2efd60c0c9fc
Andres Laguilavo <111542850+Laguilavo10@users.noreply.github.com>
2023-11-07 16:10:07 -0500

    Update package dependencies and remove unused code

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,22 @@
-import type { Request, Response } from 'express'
+import type { Request, Response, NextFunction } from 'express'
 
-export type ControllerFunction<T = void> = (req: Request, res: Response) => Promise<T>
+export type ControllerFunction<T = void> = (
+  req: Request,
+  res: Response
+) => Promise<T>
+
+export type ControllerMiddleware<T = void> = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<T>
+
+export interface Movement {
+  idMovement: string
+  userId: string
+  typeId: TypeMovement
+  createdAt: Date | string
+  description: string
+  value: number
+  methodPaymentId: MethodPayment
+}
